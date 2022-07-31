52a4a754390cfd95f1c493e0ddd4897274784a2e
francescoari <aristei37@gmail.com>
2022-07-31 12:40:22 +0200

    updated api.js

--- server/initialize.js
+++ server/initialize.js
@@ -1,54 +1,265 @@
 
 
 export default async (models) => {
-    const locationList = [
+    const eventsList = [
         {
-            name: "Cat Paradise",
-            city: "Milan"
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId: ""
         },
         {
-            name: "Cat City",
-            city: "Rome"
-        }
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId: ""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId: ""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId:""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId:""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId:""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId:""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId:""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId:""
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            type: "",
+            date: "",
+            ticket: "",
+            placeId:""
+        },
     ]
-    const location0 = await models.Location.create(locationList[0])
-    const location1 = await models.Location.create(locationList[1])
-    const catList = [
+    await models.Event.bulkCreate(eventsList)
+
+
+    const placesList = [
         {
-            name: "Cat 1",
-            breed: "Siberian",
-            description: "Details about cat 1",
-            img: "https://fs.i3lab.group/hypermedia/cats/siberian.jpg",
-            locationId: location0.id
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            map: "",
         },
         {
-            name: "Cat 2",
-            breed: "Birman",
-            description: "Details about cat 2",
-            img: "https://fs.i3lab.group/hypermedia/cats/birman.jpg",
-            locationId: location0.id
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            map: "",
         },
-        {
-            name: "Cat 3",
-            breed: "Bombay",
-            description: "Details about cat 3",
-            img: "https://fs.i3lab.group/hypermedia/cats/bombay.jpg",
-            locationId: location0.id
+                {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            map: "",
         },
-        {
-            name: "Cat 4",
-            breed: "Calico",
-            description: "Details about cat 4",
-            img: "https://fs.i3lab.group/hypermedia/cats/calico.jpg",
-            locationId: location1.id
+                {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            map: "",
         },
-        {
-            name: "Cat 5",
-            breed: "Maine Coon",
-            description: "Details about cat 5",
-            img: "https://fs.i3lab.group/hypermedia/cats/maine-coon.jpg",
-            locationId: location1.id
+                {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            map: "",
         },
     ]
-    await models.Cat.bulkCreate(catList)
+    await models.Place.bulkCreate(placesList)
+
+    const artistsList = [
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+        {
+            id: "",
+            name: "",
+            img: "",
+            description: "",
+            info: "",
+        },
+    ]
+    await models.Artist.bulkCreate(artistsList)
+
+
+    const eventsArtistsList = [
+        {
+            eventsId: 1,
+            artistsId: 1,
+        },
+    ]
+    await models.EventsArtists.bulkCreate(eventsArtistsList)
 }
