e0d1d81e0d349563f3f0213e88653591b548ae3a
francescoari <aristei37@gmail.com>
2022-07-31 11:57:51 +0200

    updated api.js

--- server/api.js
+++ server/api.js
@@ -5,100 +5,338 @@ const initialize = require('./initialize').default
 app.use(express.json())
 
 // Development
-const database = new Sequelize("postgres://postgres:postgres@localhost:5432/hyp")
+const database = new Sequelize("postgres://aiateam:aiateam@localhost:5432/summerfestival")
 
-// Production (use this code when deploying to production in Heroku)
-// const pg = require('pg')
-// pg.defaults.ssl = true
-// const database = new Sequelize(process.env.DATABASE_URL, {
-//   ssl: true,
-//   dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-// })
+// Production
 
 
+/*
+const pg = require('pg')
+pg.defaults.ssl = true
+ const database = new Sequelize(process.env.DATABASE_URL, {
+   ssl: true,
+   dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+ })
+
+*/
 
 // Function that will initialize the connection to the database
 async function initializeDatabaseConnection() {
     await database.authenticate()
-    const Cat = database.define("cat", {
+
+    const Place = database.define("Place", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        img: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        map: DataTypes.STRING,
+    }, {timestamps:false})
+
+    const Event = database.define("Event",{
+       id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        img: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        type: DataTypes.STRING,
+        pratical_info: DataTypes.STRING,
+    },{timestamps:false})
+
+    const Artist = database.define("Artist",{
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
         name: DataTypes.STRING,
         description: DataTypes.STRING,
-        breed: DataTypes.STRING,
-        img: DataTypes.STRING,
-    })
-    const Location  = database.define("location", {
-        name: DataTypes.STRING,
-        city: DataTypes.STRING,
-    })
-    Location.hasMany(Cat)
-    Cat.belongsTo(Location)
+        info: DataTypes.STRING,
+        name: DataTypes.STRING
+    },{timestamps:false})
+
+    // -----ASSOCIATIONS-----
+    // <------- 1 : N --------->
+    // PoiId column created in Service table
+    Place.hasMany(Event)
+    Event.belongsTo(Place)
+
+
+    // <-------- N : N ------->
+    // new table Events_Artists created with columns poiId and itineraryId
+
+    const Events_Artists = database.define('Events_Artists', {
+        eventId: {
+            type: DataTypes.INTEGER,
+            references: {
+            model: Event,
+            key: 'id'
+        }
+    },
+        artistId: {
+            type: DataTypes.INTEGER,
+            references: {
+            model: Artist,
+            key: 'id'
+            }
+    }
+    },{timestamp:false})
+
     await database.sync({ force: true })
     return {
-        Cat,
-        Location
+        Place,
+        Event,
+        Artist,
+        Events_Artists
     }
 }
 
 // With this line, our server will know how to parse any incoming request
 // that contains some JSON in the body
-
 const pageContentObject = {
     index: {
         title: "Homepage",
-        image: "https://fs.i3lab.group/hypermedia/images/index.jpeg",
-        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
-        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`
+        img1: "all_itineraries",
+        img2: "all_poi",
+        img3: "front_page",
+        img4: "Marathon",
+        description1:"MILAN IS A FANTASTIC CITY TO EXPLORE. WITH ALL ITS HISTORY AND MODERN CULTURE. EVERYONE COULD FIND SOMETHING INTERESTING TO DO",
+        description2: "FROM SPORTS TO MUSEUM, YOU JUST HAVE TO PICK WHICH EVENT SUITS YOU NEEDS. YOU REALLY HAVE PLENTY OF CHOOSE",
+        description3: "EVEN IF YOU'RE NOT INTO EVENTS. MILAN HAS SO MUCH MORE TO OFFER. DISCOVER SOME OF THE BEAUTIFUL PLACES HERE IN MILAN",
+        description4: "OR START A JOURNEY FOLLOWING ONE OF THE ITINERARY WE HAVE PREPARED FOR YOU",
     },
-    about: {
-        title: "About",
-        image: "https://fs.i3lab.group/hypermedia/images/about.jpeg",
-        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
-        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`
+    aboutUs: {
+        title: "Milano",
+        img1: "front_page",
+        img2: "overview",
+        description:""
     },
+    contactUs:{
+        title: "CONTACT US",
+        phone: 'PHONE: +39 333 333 3333',
+        email: 'EMAIL: aiateam@mail.it',
+        whenOpen:"We are open every week day 9:30 - 12:00, 15:00 - 18:00",
+        address: "Piazza Leonardo da Vinci, 32, 20133 Milano MI",
+        map: "https://www.google.com/maps/d/u/1/embed?mid=1rThwlxeZznE2nn-ZOFixr3rV0bVzHZE&ehbc=2E312F"
+    },
+    allPlaces:{
+        title:"ALL POINTS OF INTEREST",
+        image:"../assets/poi/all_poi.jpg",
+        introduction:""
+    },
+    allEvents:{
+        title:"ALL SERVICES",
+        img: "../assets/serviceTypes/all_services.jpg",
+        introduction: ""
+    },
+    allArtists:{
+        title:"ALL ITINERARIES",
+        img: "../assets/itineraries/all_itineraries.jpg",
+        introduction:""
+    }
 }
-
 
 async function runMainApi() {
     const models = await initializeDatabaseConnection()
     await initialize(models)
 
+    // HTTP GET calls to get single items from the list defined in the initialize.js
     app.get('/page-info/:topic', (req, res) => {
         const { topic } = req.params
         const result = pageContentObject[topic]
         return res.json(result)
     })
 
-    app.get('/cats/:id', async (req, res) => {
+    app.get('/places/:id', async (req, res) => {
         const id = +req.params.id
-        const result = await models.Cat.findOne({ where: { id }, include: [{model: models.Location}] })
+        const result = await models.Place.findOne({ where: { id } })
         return res.json(result)
     })
 
-    // HTTP GET api that returns all the cats in our actual database
-    app.get("/cats", async (req, res) => {
-        const result = await models.Cat.findAll()
+    app.get('/events/:id', async (req, res) => {
+        const id = +req.params.id
+        const result = await models.Event.findOne({ where: { id } })
+        return res.json(result)
+    })
+
+    app.get('/Artist/:id', async (req, res) => {
+        const id = +req.params.id
+        const result = await models.Artist.findOne({ where: { id } })
+        return res.json(result)
+    })
+
+    app.get('/events_of_artist/:ArtistId', async (req, res) => {
+        const artist = +req.query.ArtistId
+        const events = await models.Events_Artists.findAll({attributes: ['eventId'], where: { artistId: artist} })
+        const filtered_events = []
+        for(const element of events){
+            filtered_events.push({
+                id: element.artistId,
+            })
+        }
+        const events_list = []
+
+        for (const data of filtered_events) {
+            events_list.push(data.id)
+        }
+        console.log(events_list)
+
         const filtered = []
-        for (const element of result) {
+
+        const result = await models.Event.findAll({ where: { id: events_list } })
+        for(const element of result){
             filtered.push({
+                id: element.id,
                 name: element.name,
                 img: element.img,
-                breed: element.breed,
+                description: element.description,
+                type: element.type
+            })
+        }
+        console.log(filtered)
+        return res.json(filtered)
+    })
+
+
+    app.get('/artists_of_event/:EventId', async (req, res) => {
+        const event = +req.query.EventId
+        const artists = await models.Events_Artists.findAll({attributes: ['artistId'], where: { eventId: event} })
+        const filtered_artists = []
+        for(const element of artists){
+            filtered_artists.push({
+                id: element.eventId,
+            })
+        }
+        const artists_list = []
+
+        for (const data of filtered_artists) {
+            artists_list.push(data.id)
+        }
+        console.log(artists_list)
+
+        const filtered = []
+
+        const result = await models.Artist.findAll({ where: { id: artists_list } })
+        for(const element of result){
+            filtered.push({
                 id: element.id,
+                name: element.name,
+                img: element.img,
+                description: element.description,
+                info: element.info
+            })
+        }
+        console.log(filtered)
+        return res.json(filtered)
+    })
+
+    app.get('/events_place/:PlaceId', async(req, res) =>{
+        const place = req.query.PlaceId
+        const result = await models.Place.findAll({where: { PlaceId: place} })
+        const filtered = []
+        for(const element of result){
+            filtered.push({
+                name: element.name,
+                img: element.img
             })
         }
         return res.json(filtered)
     })
 
+
+    // HTTP GET api that returns all the cats in our actual database
+    app.get("/places", async (req, res) => {
+        const result = await models.Poi.findAll()
+        const filtered = []
+        for (const element of result) {
+            filtered.push({
+                id: element.id,
+                name: element.name,
+                description: element.description,
+                map: element.map,
+                img: element.img,
+            })
+        }
+        return res.json(filtered)
+    })
+
+    app.get("/events", async (req, res) => {
+        const result = await models.Event.findAll()
+        const filtered = []
+        for (const element of result) {
+            filtered.push({
+                id: element.id,
+                name: element.name,
+                img: element.img,
+                description: element.description,
+                type: element.type,
+                pratical_info: element.pratical_info,
+                place: element.PlaceId
+            })
+        }
+        return res.json(filtered)
+    })
+
+    app.get("/artists", async (req, res) => {
+        const result = await models.Itinerary.findAll()
+        const filtered = []
+        for (const element of result) {
+            filtered.push({
+                id: element.id,
+                name: element.name,
+                description: element.description,
+                info: element.info,
+                img: element.img,
+            })
+        }
+        return res.json(filtered)
+    })
+
+
+    app.get("/events_artists", async (req, res) => {
+        const result = await models.Events_Artists.findAll()
+        const filtered = []
+        for (const element of result) {
+            filtered.push({
+                place: element.placeId,
+                event: element.eventId
+            })
+        }
+        return res.json(filtered)
+    })
+
+
     // HTTP POST api, that will push (and therefore create) a new element in
     // our actual database
-    app.post("/cats", async (req, res) => {
+
+    app.post("/events", async (req, res) => {
         const { body } = req
-        await models.Cat.create(body);
+        await models.Event.create(body);
+        return res.sendStatus(200)
+    })
+
+    app.post("/places", async (req, res) => {
+        const { body } = req
+        await models.Place.create(body);
+        return res.sendStatus(200)
+    })
+
+    app.post("/artists", async (req, res) => {
+        const { body } = req
+        await models.Artist.create(body);
         return res.sendStatus(200)
     })
 }
 
 runMainApi()
-
-
 export default app
+
