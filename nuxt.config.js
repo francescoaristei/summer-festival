7ea71d8ff3f5b61011a3d227f9b42e8f4f1fe81a
francescoari <aristei37@gmail.com>
2022-08-14 16:45:54 +0200

    first deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // use 'https' when deploying
     //baseURL: 'https://localhost:3000'
-    baseURL: 'https://summerfestival.herokuapp.com'
+    baseURL: 'https://summer-festival.herokuapp.com'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
